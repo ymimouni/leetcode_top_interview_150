da8053011b736ec9d5bb4b54d450051dec635722
Youssef Mimouni <youssef.mimouni.53@gmail.com>
2024-05-24 19:20:01 +0100

    Bitwise AND of Numbers Range

---
+++ bit_manipulation/bitwise_and_of_numbers_range.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        shift = 0
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shift += 1
+        return left << shift
+
+
+# class Solution:
+#     def rangeBitwiseAnd(self, left: int, right: int) -> int:
+#         ans = right
+#         while ans > left:
+#             ans &= ans - 1
+#         return ans
